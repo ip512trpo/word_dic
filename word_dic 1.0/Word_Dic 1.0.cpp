1885edef649d8f23faef6aecc4b290b1ec47cee7
ip512trpo <az09212@ya.ru>
2016-05-21 01:25:57 +0600

    Добавлена функция выхода
    -По нажатию на кнопку "0" можно выйти из программы
    -Наведена небольшая красота в оформлении

--- word_dic 1.0/Word_Dic 1.0.cpp
+++ word_dic 1.0/Word_Dic 1.0.cpp
@@ -7,51 +7,46 @@
 
 using namespace std;
 
-int word_dic()
-{
-	
-    SetConsoleCP(1251);
-   
-    const int len = 30, strings = 1; //ќписываем всю хурму
+int word_dic(char input[20]){
+    const int len = 30; //ќписываем всю хурму
 	const char ch = '\n';
-	char word[20];
-	char input[20];
-	char mass[len][strings];
-      gets(input); // считываем строку с клавиатуры 
-    int len2 = strlen(input); 
+	int lenght = strlen(input); 
+	char word[lenght]; 
 	int i, k=0, q=0, m=0;
-       
-    ifstream fs("word_rus.txt"); // открываем файл .txt со словарем
-	
-	if(!fs) return 1; //≈сли ошибка открыти€ файла, то завершаем программу
-	while ((q==0)&&(m!=93393)){  //условие выхода из цикла и выход при окончании поиска
+       ifstream fs("word_rus.txt"); // открываем файл .txt со словарем
+     	if(!fs) return 1; //≈сли ошибка открыти€ файла, то завершаем программу
+       while ((q==0)&&(m!=93393)){  //условие выхода из цикла и выход при окончании поиска
         k=0; m++;
-	fs.getline(word, len-1,ch);  //считываем строку
-
-    
-
-	for (i=0;i<len2;i++){
-     if (input[i]!=word[i]){   //сравниваем по символам наши слова
+        	fs.getline(word, 30,ch);  //считываем строку
+       	for (i=0;i<lenght;i++){
+          if (input[i]!=word[i]){   //сравниваем по символам наши слова
            k++;
-             break;   //если условаие сработало, то все прервываем и начинаем заново
-                    }
-      }
+           break;   //если условаие сработало, то все прервываем и начинаем заново
+           }
+        }
     if (k==0){                    //если наше условаие ни разу не сработало (k=0), то значит слово нашлось!
            q++;       
-           cout << "Word found! That's right!\n"; //выводим сообщение, что все верно
+           cout << "---------------------------\nWord found! That's right!\n---------------------------\n\n"; //выводим сообщение, что все верно
            }
     }  
 	fs.close(); //«акрываем файл
-     
 	if (m>93392)  //если словарь кончилс€, а мы ничего не нашли, то выводим ошибку.
-	     cout << "Sorry, not found!";  
+	    cout << "---------------------------\nSorry, not found!\n---------------------------\n\n";  
 	
-	getch();
 
 }
 
-main(){
-    cout << "Please, enter the word in the Russian language..." << endl <<"-->"; // ѕриветственный текст
-    word_dic(); 
-}
-
+int main(){
+    SetConsoleCP(1251);
+    cout << "                           ..::Welcome to Word Dic 1.0!::..\n\n";
+    
+    char input[20];
+      do{
+        cout << "Please input the word or press '0' to quit \n--->";
+        cin >> input;
+        word_dic(input);}
+        while(input[0]!='0');  
+        cout << "Goodbye! :)\n";
+                Sleep(300);
+                return EXIT_SUCCESS;
+                }
